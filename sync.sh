d1b920007172b9c5d480daedb722182dc2e389c1
Marcin Floryan <mfloryan@users.noreply.github.com>
2023-01-05 12:12:11 +0000

    Validate chcksum not time

--- sync.sh
+++ sync.sh
@@ -1,1 +1,1 @@
-rsync --recursive --info=stats1,progress2 --times --compress --delete-after marcin.floryan.pl/ zorn:/srv/www/sites/floryan.pl/marcin/
+rsync --recursive --info=stats1,progress2 --checksum --compress --delete-after marcin.floryan.pl/ zorn:/srv/www/sites/floryan.pl/marcin/
