4504c67eb9ba22233c3fb92fbf1d5df538a6b1a9
Marcin Floryan <marcin.floryan@gmail.com>
2024-12-04 22:52:12 +0100

    Build with CSS

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
-rm -R site/marcin.floryan.pl/
-docker compose run build
+rm -R marcin.floryan.se/
+rm site/assets/css/*.css
+node_modules/sass/sass.js --style=compressed --no-source-map site/_sass/main.scss site/assets/css/main.css
+docker compose run --remove-orphans build 
 docker compose down
