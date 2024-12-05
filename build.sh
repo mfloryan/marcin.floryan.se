7264b2d7e87e8f8eb748cb714512f9214317a6fa
Marcin Floryan <marcin.floryan@gmail.com>
2024-12-05 16:19:58 +0100

    Correctly cleaning up CSS artefacts

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 rm -R marcin.floryan.se/
-rm site/assets/css/*.css
+rm site/assets/css/*.css*
 node_modules/sass/sass.js --style=compressed --no-source-map site/_sass/main.scss site/assets/css/main.css
 docker compose run --remove-orphans build 
 docker compose down
