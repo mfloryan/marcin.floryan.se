a983deee1cfdbdb2d11b1a40848b1e5db67b6600
Marcin Floryan <marcin.floryan@gmail.com>
2025-09-06 16:04:15 +0200

    New server

--- sync.sh
+++ sync.sh
@@ -1,1 +1,1 @@
-rsync --recursive --info=progress2,name1 --checksum --compress --delete-after marcin.floryan.se/ zorn.mm.waw.pl:/srv/www/sites/floryan.pl/marcin/
+rsync --recursive --info=progress2,name1 --checksum --compress --delete-after marcin.floryan.se/ strindberg.floryan.se:/srv/www/sites/floryan.se/marcin/
