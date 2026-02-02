9b8acec3d473c801040c6b63dde3a4cdd78bdfae
Marcin Floryan <marcin.floryan@gmail.com>
2026-02-02 22:22:57 +0100

    New host

--- sync.sh
+++ sync.sh
@@ -1,1 +1,4 @@
-rsync --recursive --info=progress2,name1 --checksum --compress --delete-after marcin.floryan.se/ strindberg.floryan.se:/srv/www/sites/floryan.se/marcin/
+rsync --recursive --info=progress2,name1 --checksum --compress --delete-after \
+  --exclude='.DS_Store' \
+  --exclude='._*' \
+  marcin.floryan.se/ klint.floryan.se:/srv/www/sites/floryan.se/marcin/
