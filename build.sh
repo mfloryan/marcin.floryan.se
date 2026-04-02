f080a1dfe81d7f488b17955314fb1d6cf406cbd5
Marcin Floryan <marcin.floryan@gmail.com>
2026-04-02 21:33:55 +0200

    Infra fix to ignore noise

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 rm -R marcin.floryan.se/
 rm site/assets/css/*.css*
-docker compose run --rm sass node node_modules/sass/sass.js --style=compressed --no-source-map site/_sass/main.scss site/assets/css/main.css
+docker compose run --rm sass node node_modules/sass/sass.js --style=compressed --no-source-map --silence-deprecation=if-function site/_sass/main.scss site/assets/css/main.css
 docker compose run --remove-orphans build
 docker compose down
